e2405497d8a85e70bb07490e80955dd011a19508
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-05-03 11:06:31 +0200

    conftest: change fixture
    Now always uses the same path

--- conftest.py
+++ conftest.py
@@ -1,9 +1,10 @@
 import pytest
+from pathlib import Path
 
 from todos import TaskManager
 
 
 @pytest.fixture
-def task_manager(tmp_path):
-    sqlite_path = tmp_path / "tasks.db"
+def task_manager():
+    sqlite_path = Path("tests.db")
     return TaskManager(sqlite_path)
